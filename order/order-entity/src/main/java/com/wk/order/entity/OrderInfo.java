bd559e5b54f86332a4e95648b1c5717ab737ab9c
wk <103420080@qq.com>
2022-01-04 20:34:53 +0800

    结构调整

--- order/order-entity/src/main/java/com/wk/order/entity/OrderInfo.java
+++ order/order-entity/src/main/java/com/wk/order/entity/OrderInfo.java
@@ -1,21 +1,15 @@
 package com.wk.order.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.Version;
+import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -27,15 +21,15 @@ import lombok.experimental.Accessors;
  */
 @Getter
 @Setter
-  @Accessors(chain = true)
-  @TableName("order_info")
+@Accessors(chain = true)
+@TableName("order_info")
 @ApiModel(value = "OrderInfo对象", description = "订单表")
 public class OrderInfo extends Model<OrderInfo> {
 
     private static final long serialVersionUID = 1L;
 
-      @TableId(value = "id", type = IdType.ASSIGN_ID)
-      private String id;
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private String id;
 
     @TableField("name")
     private String name;
@@ -52,14 +46,14 @@ public class OrderInfo extends Model<OrderInfo> {
     @TableField("discount")
     private BigDecimal discount;
 
-      @TableField(value = "crt_time", fill = FieldFill.INSERT)
-      private LocalDateTime crtTime;
+    @TableField(value = "crt_time", fill = FieldFill.INSERT)
+    private LocalDateTime crtTime;
 
     @TableField("crt_by")
     private String crtBy;
 
-      @TableField(value = "upd_time", fill = FieldFill.INSERT_UPDATE)
-      private LocalDateTime updTime;
+    @TableField(value = "upd_time", fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updTime;
 
     @TableField("upd_by")
     private String updBy;
@@ -78,7 +72,7 @@ public class OrderInfo extends Model<OrderInfo> {
 
     @Override
     public Serializable pkVal() {
-          return this.id;
-      }
+        return this.id;
+    }
 
 }
